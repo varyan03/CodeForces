662a2b26e013f215384069e3fe7c2fc0a36b52d7
Aryan <av868600@gmail.com>
2024-09-05 01:29:48 +0530

    added file for m.minimize

---
+++ A - Minimize!/solution.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+     ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int inp;
+    cin>>inp;
+    
+    while(inp>0){
+        int a;
+        int b;
+        
+        
+        cin>>a>>b;
+        
+        
+        cout<<b-a<<"\n";
+        
+        inp--;
+    }
+
+    return 0;
+}
